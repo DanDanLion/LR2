b8f4f3361166884abaec4f31536eda0b99801ac2
LR1 <nickita.sosnovskiy05@gmail.com>
2023-09-14 15:30:20 +0300

    Laborator Work 2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Завдання 1");
+        short s1, s2 = 99;
+        float f1, f2;
+        byte b = -10;
+        boolean q = true;
+        //System.out.println("s1 = "+s1);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("\nЗавдання 2");
+        s1 = (short) Math.pow(b,2);
+        System.out.println("s1 = "+s1);
+        Random random = new Random();
+        s2 = ((short) random.nextInt(10-0));
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
